1a755c6af6ce3437730033ad4e2cbfc6d16db66f
sea413 <seanmp42@gmail.com>
2016-02-17 10:10:51 -0800

    Add Index to the existing array.

--- Objects/RPS.cs
+++ Objects/RPS.cs
@@ -4,14 +4,22 @@ using System.Collections.Generic;
 
 namespace RPSNameSpace
 {
-  public class RPSclass
+  public class RPS
   {
     private int _score;
     private string _p1input;
     private string _p2input;
 
-    public RPS()
+    public RPS(string input1, string input2)
     {
+      string result;
+      string[] p1moves={"rock","paper","scissors"};
+      string[] p2moves={"rock","paper","scissors"};
+
+      _p1input = input1;
+      _p2input = input2;
+
+      
 
 
     }

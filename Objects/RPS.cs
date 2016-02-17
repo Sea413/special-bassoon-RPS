8df2fdb4641ce668e7c792469da3d90e75d94750
sea413 <seanmp42@gmail.com>
2016-02-17 08:51:33 -0800

    Add initial files for testing.

---
+++ Objects/RPS.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace RPSNameSpace
+{
+  public class RPSclass
+  {
+    private int _score;
+    private string _p1input;
+    private string _p2input;
+
+    public RPS()
+    {
+
+
+    }
+
+    public string GetInput()
+    {
+    }
+    public List<string> GetAll()
+    {
+    }
+    public void SetCandidate(string candidateInput)
+    {
+    }
+    public List<string> GetCandidate()
+    {
+    }
+
+  }
+}
